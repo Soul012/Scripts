1ba138143d3338bf88ff2cb0e5ea43b4fe48d09e
SOUL <67261074+Soul012@users.noreply.github.com>
2022-06-08 18:30:11 +0800

    Update ql_58tc.js

--- ql_58tc.js
+++ ql_58tc.js
@@ -26,6 +26,7 @@ https://magicisland.58.com/web/sign/getIndexSignInInfo url script-request-header
 [MITM]
 hostname = magicisland.58.com
 */
+const $ = new Env('58同城')
 const jsname = '58同城'
 const $ = Env(jsname)
 const logDebug = 0
