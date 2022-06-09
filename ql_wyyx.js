21cee62264adb1fc44ae1542602b9eaf70be66b5
SOUL <67261074+Soul012@users.noreply.github.com>
2022-06-09 19:23:42 +0800

    Update ql_wyyx.js

--- ql_wyyx.js
+++ ql_wyyx.js
@@ -12,7 +12,7 @@
 
 const $ = new Env("网易严选");
 const notify = $.isNode() ? require("./sendNotify") : "";
-const Notify = 1 		//0为关闭通知,1为打开通知,默认为1
+const Notify = 0 		//0为关闭通知,1为打开通知,默认为1
 const debug = 0			//0为关闭调试,1为打开调试,默认为0
 ///////////////////////////////////////////////////////////////////
 let ckStr = process.env.wyyx_data;
